7aed543313290f0bdc363d9766f6dd0485b06f33
Mason <bigmasonwang@gmail.com>
2025-07-26 19:17:48 +0930

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import apiController from './controllers/api-controller'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app
+  .use(logger())
   .get('/', (c) => {
     return c.text('Hello Hono!')
   })
